667851e554ca77c66177a957ad07f4075f04ebe3
Jayne Mast <mast.jayne@gmail.com>
2021-01-11 15:58:29 +0100

    fix bug for when no bgcolor is set

--- web/utils.js
+++ web/utils.js
@@ -1,4 +1,4 @@
-export const getColorFromBgColor = (bgColor) => {
+export const getColorFromBgColor = (bgColor = colorPrimary) => {
   const colorPrimary = '#ff3d96';
   const colorAccent = '#030059';
   if (bgColor === colorPrimary) {
