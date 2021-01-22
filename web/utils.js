ce032f2f2e3a5db8670f38efdbf8170aedd9b9a5
Jayne Mast <mast.jayne@gmail.com>
2021-01-22 12:29:12 +0100

    Fix reference

--- web/utils.js
+++ web/utils.js
@@ -1,4 +1,4 @@
-export const getColorFromBgColor = (bgColor = colorPrimary) => {
+export const getColorFromBgColor = (bgColor = '#ff3d96') => {
   const colorPrimary = '#ff3d96';
   const colorAccent = '#030059';
   const colorWhite = '#ffffff';
