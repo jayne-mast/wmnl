4508998f7a53c66c65a9c94c32b752c2d64ad82a
Jayne Mast <mast.jayne@gmail.com>
2021-02-24 20:14:10 +0100

    Add facebook pixel

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -20,6 +20,7 @@ function Layout(props) {
   const logoUrl = logo && logo.asset && logo.asset.url;
 
   const analyticsCode = 'G-0647Z46SLK';
+  const facebookPixelCode = '1842536042569596';
 
   return (
     <>
@@ -38,6 +39,24 @@ function Layout(props) {
       `,
         }}
       />
+      <!-- Facebook Pixel Code -->
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js%27);
+      fbq('init', '${facebookPixelCode}');
+      fbq('track', 'PageView');`}} />
+      <noscript><img height="1" width="1" style="display:none"
+      src={`https://www.facebook.com/tr?id=${facebookPixelCode}&ev=PageView&noscript=1`}
+      /></noscript>
+      <!-- End Facebook Pixel Code -->
       <div className={styles.container}>
         <Header title={title} navItems={mainNavigation} logo={logo} />
         <div className="content">{children}</div>
