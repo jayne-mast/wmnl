b65748f6b24e9571dc8d49525452ca8ffe70911c
Jayne Mast <mast.jayne@gmail.com>
2021-02-24 20:20:28 +0100

    Fix style

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -58,7 +58,7 @@ function Layout(props) {
         <img
           height="1"
           width="1"
-          style="display:none"
+          style={{ display: 'none' }}
           src={`https://www.facebook.com/tr?id=${facebookPixelCode}&ev=PageView&noscript=1`}
         />
       </noscript>
