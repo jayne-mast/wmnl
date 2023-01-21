fe5f3f49378ad2874f659215f807f3a8f920e11e
Marsha Jayne Verbeek-Mast <mast.jayne@gmail.com>
2023-01-21 10:20:39 +0100

    Revert "Revert "Change branding""
    This reverts commit 3caf0a3d6aae56f195210dc09a0321afaf8063d1.

--- web/utils.js
+++ web/utils.js
@@ -1,11 +1,17 @@
-export const getColorFromBgColor = (bgColor = '#ff3d96') => {
-  const colorPrimary = '#ff3d96';
-  const colorAccent = '#030059';
+export const getColorFromBgColor = (bgColor = '#f3a5e2') => {
+  const colorPrimaryOld = '#ff3d96';
+  const colorPrimary = '#f3a5e2';
+  const colorAccentOld = '#030059';
+  const colorAccent = '#5e1410';
   const colorWhite = '#ffffff';
-  if (bgColor === colorPrimary) {
+  const bgColorLowercase = bgColor.toLowerCase();
+  if (bgColorLowercase === colorPrimaryOld) {
+    return colorAccentOld;
+  }
+  if (bgColorLowercase === colorPrimary) {
     return colorAccent;
   }
-  if (bgColor === colorWhite) {
+  if (bgColorLowercase === colorWhite) {
     return '#333333';
   }
   return colorPrimary;
