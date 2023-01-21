3caf0a3d6aae56f195210dc09a0321afaf8063d1
Marsha Jayne Verbeek-Mast <mast.jayne@gmail.com>
2023-01-21 10:19:38 +0100

    Revert "Change branding"
    This reverts commit 631f61e7c52ce19e159f7332362087b8dca95b73.

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -15,7 +15,10 @@ export default class MyDocument extends Document {
       <Html lang={this.props.lang || 'nl-NL'}>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="stylesheet" href="https://use.typekit.net/zdz7wwk.css" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
           <link rel="icon" href="/static/favicon.ico" />
           <link rel="icon" href="/static/icon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/static/apple-touch-icon.png" />
--- web/utils.js
+++ web/utils.js
@@ -1,17 +1,11 @@
-export const getColorFromBgColor = (bgColor = '#f3a5e2') => {
-  const colorPrimaryOld = '#ff3d96';
-  const colorPrimary = '#f3a5e2';
-  const colorAccentOld = '#030059';
-  const colorAccent = '#5e1410';
+export const getColorFromBgColor = (bgColor = '#ff3d96') => {
+  const colorPrimary = '#ff3d96';
+  const colorAccent = '#030059';
   const colorWhite = '#ffffff';
-  const bgColorLowercase = bgColor.toLowerCase();
-  if (bgColorLowercase === colorPrimaryOld) {
-    return colorAccentOld;
-  }
-  if (bgColorLowercase === colorPrimary) {
+  if (bgColor === colorPrimary) {
     return colorAccent;
   }
-  if (bgColorLowercase === colorWhite) {
+  if (bgColor === colorWhite) {
     return '#333333';
   }
   return colorPrimary;
